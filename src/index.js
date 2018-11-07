ab50f9a47637b7ba627989396468130a22551686
bio-tarik <bio.tarik@gmail.com>
2018-11-07 16:32:47 -0200

    Adding redux and a mock api to the project

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import { render } from 'react-dom';
 import configureStore from './store/configureStore';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
+
+import { loadCourses } from './actions/courseActions';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={routes}/>
+    <Router history={browserHistory} routes={routes} />
   </Provider>,
   document.getElementById('app')
 );
